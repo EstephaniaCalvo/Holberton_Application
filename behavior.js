b45a598de5d7a33dff2d88db47bd7217435e74fe
EstephaniaCalvo <57077216+EstephaniaCalvo@users.noreply.github.com>
2020-07-15 10:25:51 -0500

    Finish template
    1. div title moved to article
    2. color active button changed to main
    3. Conditional viportwidht

--- behavior.js
+++ behavior.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    
-    //Article hight
-    var article=document.getElementById("article");
-    var heightArticle=article.offsetHeight + 120;
 
-    //Main hight
-    var mainContent=document.getElementById("main");
-    mainContent.style.height=heightArticle+"px";
+    // Conditional hight content
+    var viewportWidth = window.innerWidth;
+
+    if (viewportWidth > 800) {
+        //Article hight
+        var article=document.getElementById("article");
+        var heightArticle=article.offsetHeight + 120;
+
+        //Main hight
+        var mainContent=document.getElementById("main");
+        mainContent.style.height=heightArticle+"px";
+     };
+
 });
