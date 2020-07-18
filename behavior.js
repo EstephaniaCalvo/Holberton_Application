1ac8d2f3e65a9182635592cdb7cdbc9ef13dcbe1
EstephaniaCalvo <57077216+EstephaniaCalvo@users.noreply.github.com>
2020-07-18 12:00:22 -0500

    Index and structure
    Index finished
    subpage creation
    content height function fixed

--- behavior.js
+++ behavior.js
@@ -1,16 +1,71 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    //Adjust content height to article height if window width is greater than 800px
 
-    // Conditional hight content
-    var viewportWidth = window.innerWidth;
+    var viewportWidth;
+    var heightArticle;
+    var mainContent;
 
-    if (viewportWidth > 800) {
-        //Article hight
-        var article=document.getElementById("article");
-        var heightArticle=article.offsetHeight + 120;
+    function heightContent() {
+        // Conditional hight content
+        viewportWidth = window.innerWidth;
+        mainContent=document.getElementById("main");
 
-        //Main hight
-        var mainContent=document.getElementById("main");
-        mainContent.style.height=heightArticle+"px";
-     };
+        if (viewportWidth > 800) {
+            //Article hight
+            var article=document.getElementById("article");
+            heightArticle=article.offsetHeight + 120;
+
+            //Main hight
+            mainContent.style.height=heightArticle+"px";
+        } else{
+            mainContent.style.height="fit-content"
+        };
+    };
+
+    heightContent();
+    
+    window.addEventListener("resize", function (){
+        heightContent();
+    });   
+
+
+
+    // // Slideshow
+    // var slideIndex = 0;
+    // // showSlides();
+    // function showSlides() {
+    //     var i;
+    //     var slides = document.getElementsByClassName("image_holder");
+    //     for (i = 0; i < slides.length; i++) {
+    //         slides[i].style.display = "none";  
+    //     }
+    //     slideIndex++;
+    //     if (slideIndex > slides.length) {slideIndex = 1}    
+    //     slides[slideIndex-1].style.display = "block";  
+    //     setTimeout(showSlides, 3000);
+    // };
+
+    // var tabs=document.getElementsByClassName("tab")
+    // var tab1=tabs[0]
+    // tab1.addEventListener("mouseover", function(){
+    //     showSlides();
+    // });
+
+    
+
+    // var slideIndex = 0;
+    // showSlides();
+
+    // function showSlides() {
+    //     var i;
+    //     var slides = document.getElementsByClassName("image_holder");
+    //     for (i = 0; i < slides.length; i++) {
+    //         slides[i].style.display = "none";  
+    //     }
+    //     slideIndex++;
+    //     if (slideIndex > slides.length) {slideIndex = 1}    
+    //     slides[slideIndex-1].style.display = "block";  
+    //     setTimeout(showSlides, 3000);
+    // }
 
 });
